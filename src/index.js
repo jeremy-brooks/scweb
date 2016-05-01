b6dd6d330f2464d84891551b6f3e04912233ff5c
jeremy-brooks <jeremybrooks321@gmail.com>
2016-05-01 21:29:26 +0100

    started processing report data for dataseries

--- src/index.js
+++ src/index.js
@@ -2,20 +2,7 @@
  * Created by Jeremy on 13/04/2016.
  */
 function whenPageHasLoaded() {
-    $('[data-toggle="popover"]').popover();
-    new Locations();
-    var locationDataService = new LocationDataService();
-    addEventListener(SurfCrew.events.latestLocationDataChangedEvent, bootstrapLocationDataWithChart);
-    locationDataService.getLatestData("http://datapoint.metoffice.gov.uk/public/data/val/wxmarineobs/all/json/162081?res=hourly&key=27a379e8-5ddf-4f92-9153-d4d2ca731848");
-    locationDataService.getLatestData("http://datapoint.metoffice.gov.uk/public/data/val/wxmarineobs/all/json/162163?res=hourly&key=27a379e8-5ddf-4f92-9153-d4d2ca731848");
-    locationDataService.getLatestData("http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/354507?res=3hourly&key=27a379e8-5ddf-4f92-9153-d4d2ca731848");
-    locationDataService.getLatestData("http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/354533?res=3hourly&key=27a379e8-5ddf-4f92-9153-d4d2ca731848");
-}
-function bootstrapLocationDataWithChart(event) {
-    if (event && event.detail && event.detail instanceof OldLocation) {
-        var location = event.detail;
-        $("#chart-"+location.id).highcharts(location.getLatestData());
-    }
+    createHybridChart();
 }
 function createHybridChart(){
     var url = "http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/354507?res=3hourly&key=27a379e8-5ddf-4f92-9153-d4d2ca731848";
@@ -24,6 +11,13 @@ function createHybridChart(){
         if (http.readyState === 4 && http.status === 200){
             var parsedData = JSON.parse(http.responseText);
             var location = new DataPointLocation(parsedData);
+            var options = {
+                chart: {
+                    type: 'spline'
+                },
+                series: location.seriesData
+            };
+            $("#chart-hybrid").highcharts(options);
         }
     };
     http.open("GET", url, true);
--- src/surf/locations/model/location.js
+++ src/surf/locations/model/location.js
@@ -6,60 +6,72 @@ var DataPointLocation = function () {
     this.name = "unknown";
     this.type = "unknown";
     this.dataDate = null;
-    this.data = null;
+    this.seriesData = null;
     this.weatherParametersAvailable = null;
-    this.dataSeries = null;
 
     (function (data) {
         var locationData = null;
         var metaData = null;
-        var data = null;
-        if (data){
-            if (data.SiteRep){
-                if (data.SiteRep.DV && data.SiteRep.Wx && data.SiteRep.DV.Location){
-                    try {
-                        metaData = data.SiteRep.DV;
-                        locationData = data.SiteRep.DV.Location;
-                        data = locationData.Period;
-                        this.id = locationData.i;
-                        this.name = locationData.name;
-                        this.dataDate = metaData.dataDate;
-                        this.type = metaData.type;
-                        this.weatherParametersAvailable = data.SiteRep.Wx.Param;
-                        this.data = [];
+        var period = null;
 
-                        for (var dataIndex = 0, dataItem = null; dataItem = data[dataIndex]; dataIndex++){
-                            if (dataItem.Rep){
-                                for (var repIndex = 0, rep = null; rep = dataItem.Rep[repIndex]; repIndex++){
-                                    for (var paramIndex = 0, param = null; param = this.weatherParametersAvailable[paramIndex]; paramIndex++){
-                                        if (rep[param.name]){
-                                            this.data.push({
-                                                x: dataItem.value,
-                                                y: param.name,
-                                                name: param.$
-                                            });
-                                        }
-                                    }
-                                    // var dataPoint = {
-                                    //     x: dataItem.value,
-                                    //     y: rep
-                                    // };
-                                    // this.data.push[dataPoint];
+        if (this.isDataValid(data)){
+            try {
+                metaData = data.SiteRep.DV;
+                locationData = data.SiteRep.DV.Location;
+                period = locationData.Period;
+                this.id = locationData.i;
+                this.name = locationData.name;
+                this.dataDate = metaData.dataDate;
+                this.type = metaData.type;
+                this.weatherParametersAvailable = data.SiteRep.Wx.Param;
+                this.seriesData = [];
+
+                for (var paramIndex = 0, param = null; param = this.weatherParametersAvailable[paramIndex]; paramIndex++){
+                    this.seriesData.push({
+                        units: param.units,
+                        type: 'spline',
+                        name: param.name,
+                        data: [],
+                        turboThreshold: 0
+                    });
+                }
+
+                for (var dataIndex = 0, dataItem = null; dataItem = period[dataIndex]; dataIndex++){
+                    if (dataItem.Rep){
+                        for (var repIndex = 0, rep = null; rep = dataItem.Rep[repIndex]; repIndex++){
+                            for (var seriesIndex = 0, series = null; series = this.seriesData[seriesIndex]; seriesIndex++){
+                                if (rep[series.name]){
+                                    series.data.push({
+                                        x: Date.parse(dataItem.value),
+                                        y: Number(rep[series.name])
+                                    });
                                 }
                             }
                         }
-
-                    } catch (error) {
-                        console.error("Something went wrong getting location detail from data|" + error);
                     }
-                } else {
-                    console.error("No location found in DV");
                 }
-            } else {
-                console.warning("No SiteRep found in data to set new location");
+
+                var a = null;
+
+            } catch (error) {
+                console.error("Something went wrong getting location detail from data|" + error);
             }
-        } else {
-            console.warning("No data found to set new location");
         }
     }).apply(this, arguments);
+};
+DataPointLocation.prototype.isDataValid = function (data) {
+    if (data){
+        if (data.SiteRep){
+            if (data.SiteRep.DV && data.SiteRep.Wx && data.SiteRep.Wx.Param && data.SiteRep.DV.Location){
+                return true;
+            } else {
+                console.warn("No location found in DV");
+            }
+        } else {
+            console.warn("No SiteRep found in data to set new location");
+        }
+    } else {
+        console.warn("No data found to set new location");
+    }
+    return false;
 };
