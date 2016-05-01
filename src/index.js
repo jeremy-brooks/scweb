a7b5eb8e4a9f0883c96680c38a8fe30a93d1daa4
jeremy-brooks <jeremybrooks321@gmail.com>
2016-05-01 23:55:05 +0100

    started to extract common code for running against other sites

--- src/index.js
+++ src/index.js
@@ -2,16 +2,15 @@
  * Created by Jeremy on 13/04/2016.
  */
 function whenPageHasLoaded() {
-    createHybridChart();
+    createHybridChart("#chart-hybrid", "http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/354507?res=3hourly&key=27a379e8-5ddf-4f92-9153-d4d2ca731848");
 }
-function createHybridChart(){
-    var url = "http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/354507?res=3hourly&key=27a379e8-5ddf-4f92-9153-d4d2ca731848";
+function createHybridChart(elementId, url){
     var http = new XMLHttpRequest();
     http.onreadystatechange = function () {
         if (http.readyState === 4 && http.status === 200){
             var parsedData = JSON.parse(http.responseText);
             var location = new DataPointLocation(parsedData);
-            $("#chart-hybrid").highcharts(location.options);
+            $(elementId).highcharts(location.options);
         }
     };
     http.open("GET", url, true);
