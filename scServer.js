0be3fe4df58bf4cbcace4d0c8bba781664eb62fe
jeremy-brooks <jeremy.brooks@mapofmedicine.com>
2016-07-29 16:08:37 +0100

    added express server

---
+++ scServer.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var path = require("path");
+
+var port = 3000;
+var app = express();
+var webSiteRoot = path.join(__dirname, "/");
+
+app.use(express.static(webSiteRoot));
+
+app.listen(port);
+
+console.log("started on port %s", port);
